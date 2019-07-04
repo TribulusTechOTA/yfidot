ba4f01035ce9574c599f08805b4d9175f9ae0d6c
vidya <vidya@tribulustech.com>
2019-07-04 23:44:12 +0530

    first version

---
+++ main/main.py
@@ -0,0 +1,25 @@
+fileVer='201907041350'
+print('main ver ' + fileVer)
+try:
+    from ota_updater import OTAUpdater
+except Exception as ex:
+    tx = str(ex)
+    print (tx)
+
+#from ota_update.main.ota_updater import OTAUpdater
+
+
+def download_and_install_update_if_available():
+    ota_updater = OTAUpdater('https://github.com/TribulusTechOTA/yfidot.git')
+    ota_updater.download_and_install_update_if_available('Tribulus', 'Redhat@345')
+
+def start():
+    #import smartplug
+    #smartplug.listen()
+    print("Hello 1")
+
+def boot():
+    download_and_install_update_if_available()
+    start()
+
+#boot()
